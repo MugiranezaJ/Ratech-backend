66549399e40c27761dc984cc5a127af9fa5487df
MugiranezaJ <jackswalter7@gmail.com>
2022-07-13 22:32:30 +0200

    fixing login route

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,5 @@ from .views import LoginView, Register
 
 urlpatterns = [
     path('register', Register.as_view(), name="register"),
-    path('login/', LoginView.as_view(), name="login"),
+    path('login', LoginView.as_view(), name="login"),
 ]
